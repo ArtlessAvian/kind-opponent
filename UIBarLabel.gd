5d25350995234eb88ce36633f13c8da02caf2597
Kyle Wong <kylewonga@gmail.com>
2019-04-13 14:13:39 -0700

    Bars should update themselves

--- UIBarLabel.gd
+++ UIBarLabel.gd
@@ -1,22 +1,17 @@
-extends ColorRect
+extends RichTextLabel
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export(String) var type_prefix
 
 # Called when the node enters the scene tree for the first time.
-
-export(float) var perc = 1
 func _ready():
-	set_value(perc)
-	
+	pass # Replace with function body.
 
+func update_value(text):
+	self.text = type_prefix + text
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-	#self.set_anchor_and_margin(MARGIN_RIGHT,perc,0,false)
-	
-	
-func set_value(percent):
-	print(percent)
-	perc = percent
-	self.set_anchor_and_margin(MARGIN_RIGHT,perc,0,false)
+#	pass
