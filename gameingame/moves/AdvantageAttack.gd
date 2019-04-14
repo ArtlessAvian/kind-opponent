c3b9df59fb2f740e9ca19b5989aa14ff43224284
syy1125 <syy1125@hotmail.com>
2019-04-14 10:13:15 -0700

    Perfect opportunity adv attack only drains half

--- gameingame/moves/AdvantageAttack.gd
+++ gameingame/moves/AdvantageAttack.gd
@@ -4,8 +4,8 @@ export (float, 0, 2) var efficiency = 0.8
 export (float, 0, 2) var regen = 0.03
 
 func run(user, opponent, opportunity):
-	opponent.take_damage(user.advantage * efficiency * (2 if opportunity else 1))
-	user.advantage = 0
+	opponent.take_damage(user.advantage * efficiency)
+	user.advantage = (user.advantage / 2) if opportunity else 0
 	user.get_healed(regen * (user.max_health - user._health))
 
 func available(user):
